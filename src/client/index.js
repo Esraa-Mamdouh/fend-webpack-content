b301825779378a7d9c7a3af941ddb03bff286be6
Esraa-Mamdouh <esraa.ali9798@gmail.com>
2021-09-30 21:28:23 +0200

    first

--- src/client/index.js
+++ src/client/index.js
@@ -4,3 +4,5 @@ import { handleSubmit } from './js/formHandler'
 console.log(checkForName);
 
 alert("I EXIST")
+alert("Esraa!!")
+alert("new alert for build test")
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
@@ -18,6 +19,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        
     ]
 }

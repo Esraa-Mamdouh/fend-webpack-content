1e2e283390eab5a150b2b3e63f48a9685617f0a3
Esraa-Mamdouh <esraa.ali9798@gmail.com>
2021-09-30 12:43:56 +0200

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+import img from '../../index.jpg';
+
+console.log(checkForName);
+
+
+alert("Hi Esraa!")
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,23 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                },
+            {
+            //test: '/\.(png|jpe?g|gif)$/',
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                  loader: 'file-loader',
+                },
+            ]  
+            },
+        ]
+}
 }
